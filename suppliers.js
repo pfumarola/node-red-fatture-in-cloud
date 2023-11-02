8beb24bed4a1ae82f983b85baea22a23ba822b86
pfumarola <paolo96fumarola@gmail.com>
2023-11-02 18:07:40 +0100

    Fixed misspelled methods name

--- suppliers.js
+++ suppliers.js
@@ -7,7 +7,7 @@ module.exports = function(RED) {
 		node.on('input', async function(msg) {
 			node.status({fill:"blue",shape:"dot",text:"Requesting"});
 			try {
-				msg.payload = await listsupplierss(msg.token);
+				msg.payload = await listsuppliers(msg.token);
 				node.status({})
 			} catch (error) {
 				node.status({fill:"red",shape:"ring",text:"Error"});
@@ -18,7 +18,7 @@ module.exports = function(RED) {
 	}
 	RED.nodes.registerType("suppliers",GeneralNode);
 	
-	async function listsupplierss(accessToken) {
+	async function listsuppliers(accessToken) {
 		let defaultClient = fattureInCloudSdk.ApiClient.instance;
 		let OAuth2AuthenticationCodeFlow = defaultClient.authentications['OAuth2AuthenticationCodeFlow'];
     OAuth2AuthenticationCodeFlow.accessToken = accessToken;
@@ -28,9 +28,9 @@ module.exports = function(RED) {
 		 let userCompaniesResponse = await userApiInstance.listUserCompanies();
 		 let firstCompanyId = userCompaniesResponse.data.companies[0].id;
 
-		 // Retrieve the list of the supplierss
-		 let suppliersApiInstance = new fattureInCloudSdk.supplierssApi();
-		 let companysuppliers = await suppliersApiInstance.listsupplierss(firstCompanyId);
+		 // Retrieve the list of the suppliers
+		 let suppliersApiInstance = new fattureInCloudSdk.suppliersApi();
+		 let companysuppliers = await suppliersApiInstance.listsuppliers(firstCompanyId);
 		 
 		 return JSON.stringify(companysuppliers.data)
 	}
